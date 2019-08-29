347b181bd32cb5946d82939f1545efccd9b53bcf
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-29 15:42:53 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,2 +1,7 @@
-#!/bin/bash
-echo "$1, StackStorm!"
+import sys
+
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+	def run(self,greeting):
+		print(greeting, "Stackstorm")
